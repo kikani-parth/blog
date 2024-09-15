751dd8d323697c7dbdaf901bb5cbc435316d29ef
Parth Kikani <parthkikani02@gmail.com>
2024-09-15 12:24:39 +0300

    add functionality to delete a blogPost

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,66 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
+import { Ionicons } from '@expo/vector-icons/';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
   return (
     <View>
-      <Text>index screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
-        keyExtractor={(blogPost) => blogPost.title}
+        keyExtractor={(blogPost) => blogPost.id}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Ionicons name="trash" style={styles.deleteIcon} />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    margin: 15,
+    borderColor: 'gray',
+    borderRadius: 10,
+    backgroundColor: 'white',
+
+    // Shadow for iOS
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+
+    // Shadow for Android
+    elevation: 5,
+  },
+  title: {
+    fontSize: 18,
+  },
+  deleteIcon: {
+    fontSize: 24,
+    color: 'red',
+  },
+});
 
 export default IndexScreen;
